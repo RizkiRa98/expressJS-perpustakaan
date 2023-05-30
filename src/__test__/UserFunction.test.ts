b38a631502b486b076fd78995b0b40169dc275b4
RizkiRa98 <rizkiramadhan350@gmail.com>
2023-05-30 15:27:29 +0700

    Update 30 may/issue error di updateUser case 4

--- src/__test__/UserFunction.test.ts
+++ src/__test__/UserFunction.test.ts
@@ -1,28 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable max-len */
 import {createUser} from '../controllers/Users/createUsers';
+import {getUser, getUserById} from '../controllers/Users/getUsers';
+import {deleteUser} from '../controllers/Users/deleteUsers';
+import {updateUser} from '../controllers/Users/updateUsers';
 import {Request, Response} from 'express';
 import bcrypt from 'bcrypt';
 import validator from 'validator';
 import Users from '../models/userModel';
-import {getUser} from '../controllers/Users/getUsers';
-
-// Mocking request dan response untuk objects
-const mockRequest = (params = {}): Request =>
-  ({
-    ...params,
-  } as Request);
-
-const mockResponse = (): Response => {
-  const res: Response = {} as Response;
-  res.json = jest.fn().mockReturnValue(res);
-  res.status = jest.fn().mockReturnValue(res);
-  return res;
-};
+import {WhereOptions} from 'sequelize';
 
 // Mock lokasi models
 jest.mock('../models/userModel', () => ({
   findOne: jest.fn(),
   create: jest.fn(),
+  destroy: jest.fn(),
+  update: jest.fn(),
 }));
 
 // Create User Function
@@ -163,8 +156,17 @@ describe('createUser', () => {
 
 // End Create User Function
 
-// Get User Function
+// Get All User Function
 describe('getUser', () => {
+  let req: Request;
+  let res: Response;
+
+  beforeEach(() => {
+    req = {} as Request;
+    res = {
+      json: jest.fn(),
+    } as unknown as Response;
+  });
   it('should return all users data', async () => {
     const mockUsers = [
       {
@@ -182,18 +184,290 @@ describe('getUser', () => {
         createdAt: new Date(),
       },
     ];
-
-    (Users.findAll as jest.Mock).mockResolvedValueOnce(mockUsers);
-
-    const req = mockRequest();
-    const res = mockResponse();
+    Users.findAll = jest.fn().mockResolvedValue(mockUsers);
 
     await getUser(req, res);
 
     // Ekpetasi yang diharapkan
     expect(Users.findAll).toHaveBeenCalled();
-    expect(res.status).toHaveBeenCalledWith(200);
+    // expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith(mockUsers);
   });
 });
+// END Get All User
+
+// Get Users by Id
+describe('getUserById', () => {
+  let req: Request;
+  let res: Response;
+
+  beforeEach(() => {
+    req = {
+      params: {id: '1'},
+    } as unknown as Request;
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+  });
+
+  // Case 1 jika user ada
+  it('should return users data by id requested', async () => {
+    const mockUsers = {
+      id: '1',
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      role: 'super admin',
+      createdAt: new Date(),
+    };
+
+    Users.findOne = jest.fn().mockResolvedValue(mockUsers);
+
+    await getUserById(req, res);
+
+    // Ekspetasi yang diharapkan
+    expect(Users.findOne).toHaveBeenCalledWith({
+      attributes: ['id', 'name', 'email', 'role', 'createdAt'],
+      where: {id: req.params.id} as WhereOptions<Users>,
+    });
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(mockUsers);
+  });
+
+  // Case 2 jika user tidak ada
+  it('should return status 404 it user is not found', async () => {
+    Users.findOne = jest.fn().mockResolvedValue(null);
+
+    await getUserById(req, res);
+
+    // ekspetasi yang diharapkan
+    expect(Users.findOne).toHaveBeenCalledWith({
+      attributes: ['id', 'name', 'email', 'role', 'createdAt'],
+      where: {id: req.params.id} as WhereOptions<Users>,
+    });
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({
+      msg: 'User tidak ditemukan',
+    });
+  });
+});
+// END Get User By Id
+
+// Delete User By Id
+// interface custom request untuk request role dan params id
+interface MockRequest extends Request {
+  role: 'super admin' | 'admin';
+  params: {id: string};
+}
+describe('deleteUser', () => {
+  let req: MockRequest;
+  let res: Response;
+
+  beforeEach(() => {
+    req = {
+      params: {id: '1'},
+      role: 'super admin',
+    } as MockRequest;
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+  });
+
+  // Case 1 berhasil delete user jika user ada
+  it('should delete user and success message if user exist and requester is super admin', async () => {
+    const user = {id: 1};
+    (Users.findOne as jest.Mock).mockResolvedValue(user);
+
+    await deleteUser(req, res);
+
+    expect(Users.findOne).toHaveBeenCalledWith({
+      where: {id: req.params.id} as WhereOptions<Users>,
+    });
+    expect(Users.destroy).toHaveBeenCalledWith({
+      where: {id: user.id} as WhereOptions<Users>,
+    });
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      msg: `User dengan Id ${req.params.id} berhasil dihapus`,
+    });
+  });
+
+  // Case 2 jika user yang request tidak ditemukan
+  it('should send 404 status and "User not found" message if user does not exist', async () => {
+    // JIka hasil mock kosong gunakan null
+    (Users.findOne as jest.Mock).mockResolvedValue(null);
+
+    await deleteUser(req, res);
+
+    // ekspetasi yang diharapkan
+    expect(Users.findOne).toHaveBeenCalledWith({
+      where: {id: req.params.id},
+    });
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({
+      msg: `User dengan id ${req.params.id} tidak ditemukan`,
+    });
+  });
+
+  // case 3 jika role request bukan super admin
+  it('should return status 403 and "Access denied" message if requester is not super admin', async () => {
+    // User yang di request dengan id 1
+    const user = {id: 1};
+    (Users.findOne as jest.Mock).mockResolvedValue(user);
+
+    // mock role yang di request
+    req.role = 'admin';
+
+    await deleteUser(req, res);
+
+    // Ekspetasi yang diharapkan
+    expect(res.status).toHaveBeenCalledWith(403);
+    expect(res.json).toHaveBeenCalledWith({
+      msg: 'Akses ditolak! Role admin tidak bisa menghapus user.',
+    });
+  });
+});
+// END delete user
+
+// Update User
+describe('updateUser', () => {
+  let req: Request;
+  let res: Response;
+
+  beforeEach(() => {
+    req = {
+      params: {id: '1'},
+      body: {},
+    } as unknown as Request;
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  // Case 1 Jika user yang di request tidak ada
+  it('should return status 404 and "Users not found" message if user is not found', async () => {
+    // Mock User.findOne untuk return null
+    // Nilai null menindikasi user tidak ditemukan
+    (Users.findOne as jest.Mock).mockResolvedValue(null);
+
+    await updateUser(req, res);
+
+    expect(Users.findOne).toHaveBeenCalledWith({
+      where: {id: req.params.id} as WhereOptions<Users>,
+    });
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({
+      msg: `User dengan id ${req.params.id} tidak ditemukan`,
+    });
+  });
+
+  // Case 2 Jika email yang di request format nya invalid
+  it('should return status 400 and "Invalid email format" message if email format is invalid ', async () => {
+    // Mock User.findOne untuk return data user
+    (Users.findOne as jest.Mock).mockResolvedValue({
+      id: '1',
+    });
+
+    req.body.email = 'invalid-email';
+
+    await updateUser(req, res);
+
+    expect(Users.findOne).toHaveBeenCalledWith({
+      where: {id: req.params.id} as WhereOptions<Users>,
+    });
+    expect(validator.isEmail).toHaveBeenCalledWith(req.body.email);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      msg: 'Format Email Salah',
+    });
+  });
+
+  // Case 3 Jika email sudah digunakan
+  it('should return status 400 and "Email is already used" message if user is already used', async () => {
+    jest.spyOn(Users, 'update');
+    const existingUser = {
+      id: '2',
+      email: 'existing-email@example.com',
+    };
+
+    // mock Users.findOne untuk return user yang memiliki email yang sama
+    (Users.findOne as jest.Mock).mockResolvedValue(existingUser.email);
+
+    req.body.email = existingUser.email;
+
+    await updateUser(req, res);
+
+    expect(Users.findOne).toHaveBeenCalledWith({
+      where: {id: req.params.id} as WhereOptions<Users>,
+    });
+    expect(Users.findOne).toHaveBeenCalledWith({
+      where: {email: req.body.email},
+    });
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      msg: `Email ${req.body.email} sudah digunakan`,
+    });
+  });
+
+  // Case 4 update user berhasil
+  it('should update user and return status 200 and success message', async () => {
+    jest.spyOn(Users, 'update');
+    const user = {
+      id: '1',
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: 'password',
+      role: 'admin',
+    };
+    const updatedUser = {
+      id: '1',
+      name: 'Updated Name',
+      email: 'updated-email@example.com',
+      password: 'password',
+      role: 'admin',
+    };
+
+    // Mock Users.findOne to return user data
+    (Users.findOne as jest.Mock).mockResolvedValue(user);
+
+    // Request body
+    req.body.name = updatedUser.name;
+    req.body.email = updatedUser.email;
+    req.body.password = 'new-password';
+    req.body.confPassword = 'new-password';
+    req.body.role = updatedUser.role;
+
+    await updateUser(req, res);
+
+    // Expectations
+    expect(Users.findOne).toHaveBeenCalledWith({
+      where: {id: req.params.id} as WhereOptions<Users>,
+    });
+    expect(Users.update).toHaveBeenCalledWith(
+      {
+        name: updatedUser.name,
+        email: updatedUser.email,
+        password: 'new-password',
+        role: updatedUser.role,
+      },
+      {
+        where: {
+          id: user.id,
+        } as WhereOptions<Users>,
+      },
+    );
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      msg: `User dengan id ${req.params.id} berhasil di update`,
+    });
+  });
+});
+
+// END update user
 
